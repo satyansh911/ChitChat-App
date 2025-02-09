b07a2f638620aa26d621b9b70f5fe17bd63c5a40
satyansh911 <143713180+satyansh911@users.noreply.github.com>
2025-02-09 13:25:22 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 import daisyui from "daisyui";
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default  {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Adjust based on your file structure
   theme: {
     extend: {
