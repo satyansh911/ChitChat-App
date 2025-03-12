0c3fbf7ad5aa20b26b2ce3063099063147672a01
satya <satyanshofficial@gmail.com>
2025-03-13 03:17:20 +0530

    Adjusted Lower Space Issue in UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-xl w-[1400px]  h-[calc(100vh-8rem)]">
+        <div className="bg-base-100 rounded-lg shadow-xl w-[1400px]  h-[calc(104vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
 
